db076d0bd601d9d0c44b0316e129cf9befba8eca
pedroara <araujo.peo@gmail.com>
2019-11-21 13:08:02 -0300

    fachada em andamento

--- src/sistema/claudia/dados/RepositorioUsuario.java
+++ src/sistema/claudia/dados/RepositorioUsuario.java
@@ -9,9 +9,12 @@ public class RepositorioUsuario {
 	
 	private ArrayList<Usuario> repoUser;
 	
-	
 	public RepositorioUsuario() {
 		repoUser = new ArrayList<Usuario>();
+	}
+	
+	public ArrayList<Usuario> getRepoUser(){
+		return repoUser;
 	}
 	
 	public void adicionarUsuario(Usuario user) {
@@ -36,6 +39,7 @@ public class RepositorioUsuario {
 //		}
 //		return null;
 //	}
+	
 	public Usuario getUsuarioPeloNome(String nome) {
 		if(repoUser != null) {
 			for(Usuario u: repoUser) {
@@ -47,5 +51,16 @@ public class RepositorioUsuario {
 		return null;
 	}
 	
+	@SuppressWarnings("unused")
+	private Usuario login (String nomeDoUsuario,  int senha){
+		for(int  i = 0; i < repoUser.size() ; i++) {
+			if(nomeDoUsuario.equals(repoUser.get(i).getNome())) {
+				if(senha == repoUser.get(i).getSenha()) {
+					return repoUser.get(i);
+				}
+			}
+		}
+		return repoUser.get(0);
+	}
 	
 }
--- src/sistema/claudia/negocio/ControladorUsuario.java
+++ src/sistema/claudia/negocio/ControladorUsuario.java
@@ -4,6 +4,7 @@ import sistema.claudia.dados.RepositorioUsuario;
 import sistema.claudia.exceptions.CampoVazioException;
 
 public class ControladorUsuario {
+	
 	private RepositorioUsuario users;
 	
 	public ControladorUsuario(RepositorioUsuario users) {
@@ -15,6 +16,25 @@ public class ControladorUsuario {
 			users.adicionarUsuario(user);
 		} else {
 			throw new IllegalArgumentException("Usuário inválido ou vazio.");
+		}
+	}
+	
+	public Usuario loginDeUsuario(String nick, int senha) throws CampoVazioException{
+		
+		if(nick != null) {
+			
+			for(int  i = 0; i < users.getRepoUser().size() ; i++) {
+				if(nick.equals(users.getRepoUser().get(i).getNome())) {
+					if(senha == users.getRepoUser().get(i).getSenha()) {
+						return users.getRepoUser().get(i);
+					}
+				}
+			}	
+			
+		}if(nick == null) {
+			throw new CampoVazioException("Nome");
+		} else {
+			throw new IllegalArgumentException("Usuário não existe.");
 		}
 	}
 	
