dc9d6a82651d0fd825b6864b5dee3f91d26ced49
izab3lle <51674137+izab3lle@users.noreply.github.com>
2019-11-21 01:09:13 -0300

    Controlador de calendário
    o git tá me bugando

---
+++ src/sistema/claudia/negocio/ControladorCalendario.java
@@ -0,0 +1,55 @@
+package sistema.claudia.negocio;
+
+import java.time.LocalDateTime;
+import sistema.claudia.dados.Calendario;
+import sistema.claudia.exceptions.DataInvalidaException;
+import sistema.claudia.exceptions.EventoJaExistenteException;
+import sistema.claudia.negocio.Evento;
+import java.time.temporal.ChronoField;
+
+public class ControladorCalendario {
+	
+	private Calendario calendario;
+	
+	public ControladorCalendario(Calendario calendario) {
+		this.calendario = calendario;
+	}
+	
+	public void cadastrar(Evento e) throws EventoJaExistenteException {
+		if(e == null) {
+			throw new IllegalArgumentException("Evento vazio");
+		} else {
+			if(!calendario.existe(e)) {
+				calendario.adicionar(e.getNome(),
+						e.getDescricao(),
+						e.dataToString(e.getDataHoraInicio()),
+						e.dataToString(e.getDataHoraFim()));
+			} else {
+				throw new EventoJaExistenteException(e.getNome());
+			}
+		}
+	}
+	
+	public void remover(Evento e) throws EventoJaExistenteException {
+		if(e != null && calendario.existe(e)) {
+			calendario.remover(e);
+		} else {
+			throw new IllegalArgumentException("Evento não existe");
+		}
+	}
+	
+	public Evento[] buscarPorData(LocalDateTime data) throws DataInvalidaException {
+		if(data.isSupported(ChronoField.DAY_OF_MONTH)
+				&& data.isSupported(ChronoField.MONTH_OF_YEAR)
+				&& data.isSupported(ChronoField.YEAR)) {
+			return calendario.buscarPorData(data);
+		} else {
+			throw new DataInvalidaException(data);
+		}
+	}
+	
+	public boolean existe(Evento e) {
+		return calendario.existe(e);
+	}
+
+}
