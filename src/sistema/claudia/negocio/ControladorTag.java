9fc1f7f42f4fed48b6e3639c65b513cc044df3e3
izab3lle <ohmadorito@gmail.com>
2019-11-21 04:08:56 -0300

    Controlador de tags

--- src/sistema/claudia/negocio/ControladorTag.java
+++ src/sistema/claudia/negocio/ControladorTag.java
@@ -14,12 +14,42 @@ public class ControladorTag {
 	public void adicionarTag(String nome) throws CampoVazioException {
 		if(nome != null && tags.buscarTagPeloNome(nome) == null) {
 			tags.adicionarTag(nome);
+		}
+		if(tags.buscarTagPeloNome(nome) == null) {
+			throw new IllegalArgumentException("Tag não existe.");
 		} else {
 			throw new CampoVazioException("Nome");
 		}
 	}
 	
-	public void removerTag(int index, Tag tag) throws CampoVazioException {
-		
+	public void removerTag(Tag tag) throws CampoVazioException {
+		if(tag != null && tags.buscarTagPeloNome(tag.getNome()) != null) {
+			tags.removerTag(tag);
+		}else {
+			throw new CampoVazioException("Nome");
+		}
+	}
+	
+	public void buscarPeloNome(String nome) throws CampoVazioException {
+		if(nome != null) {
+			tags.buscarTagPeloNome(nome);
+		} else {
+			throw new CampoVazioException("Nome");
+		}
+	}
+	
+	public void listarTagsExistentes() {
+		tags.listarTagsExistentes();
+	}
+	
+	public void adicionarEventoNaTag(Tag tag, Evento evento) throws CampoVazioException{
+		if(tags.buscarTagPeloNome(tag.getNome()) != null) {
+			tags.adicionarEventoNaTag(tag, evento);
+		}
+		if(tag.getNome() == null) {
+			throw new CampoVazioException("Nome");
+		} else {
+			throw new IllegalArgumentException("Tag não existe.");
+		}
 	}
 }
