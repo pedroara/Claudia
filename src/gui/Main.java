d5ae80f8d0252778840fbe7bd0ff463d91a87fc9
MariellyLins <mariellylinsdasilva@gmail.com>
2019-11-25 11:25:25 -0300

    icone adicionado

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -27,6 +28,9 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Claudia");
+			
+			primaryStage.getIcons().add(new Image("file:details/calendar.png"));
+			
 			primaryStage.show();
 			
 		} catch(IOException e) {
